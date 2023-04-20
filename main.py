bf8c1174ba45ea8c7764a27f9a96737a1ebebc3b
AwangYe <110916067+AwangYe@users.noreply.github.com>
2023-04-20 16:38:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,9 +26,18 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 
 for i in range(len(user_ids)):
+#     data = {
+#         "title1": {"value": "今日日期", "color": get_random_color()},
+#         "content1": {"value": "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈", "color": get_random_color()}
+#     }
     data = {
-        "title1": {"value": "今日日期", "color": get_random_color()},
-        "content1": {"value": "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈", "color": get_random_color()}
+        "title1": {"value": "title", "color":"#A8A8A8"},
+		"title2": {"value": "通知内容:\t\t\t\t", "color":"#A8A8A8"},
+		"title3": {"value": "通知时间:\t\t\t\t", "color":"#A8A8A8"},
+		"title4": {"value": "备注:\t\t\t\t", "color":"#A8A8A8"},
+		"content1": {"value": "` + Msg + `\n"},
+		"content2": {"value": "` + time + `\n"},
+		"content3": {"value":"本次推送由yangqu支持\n"}
     }
     res = wm.send_template(user_ids[i], template_ids[i], data)
     print(res)
