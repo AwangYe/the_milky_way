284562c588ef99c6575f27039d66d1a81ffc9204
AwangYe <110916067+AwangYe@users.noreply.github.com>
2023-04-24 10:58:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import math
 import random
 import requests
+import urllib.parse
 
 from datetime import date, datetime
 from wechatpy import WeChatClient
@@ -30,18 +31,27 @@ msg_title = '测试标题'
 msg_content = '测试内容测试内容测试内容测试内容测试内容测试内容测试内容测试内容测试内容测试内容'
 
 for i in range(len(user_ids)):
+    url = "http://awangye.github.io/?"
+    values={}
+    values['title'] = msg_title
+    values['date'] = today
+    values['cont'] = msg_content
+    url = url + urllib.parse.urlencode(values)
+    
+    data = {
+        "title1": {"value": "{}".format(msg_title), "color":"#A8A8A8"},
+	    "title2": {"value": "通知内容:\t\t\t\t", "color":"#A8A8A8"},
+        "title3": {"value": "通知时间:\t\t\t\t", "color":"#A8A8A8"},
+		"title4": {"value": "备注:\t\t\t\t", "color":"#A8A8A8"},
+		"content1": {"value": "{}\n".format(msg_content)},
+		"content2": {"value": "{}\n".format(today)},
+		"content3": {"value":"回复任意消息重新启用文本通知模式\n"}
+    }
+    res = wm.send_template(user_ids[i], template_ids[i], data, url)
+    print(res)
+    
     try:
         res1 = wm.send_text(user_ids[i], "打卡成功\n内容1\n内容2")
     except WeChatClientException as e:
     	if e.errcode == 45015:
-    		data = {
-        		"title1": {"value": "{}".format(msg_title), "color":"#A8A8A8"},
-			"title2": {"value": "通知内容:\t\t\t\t", "color":"#A8A8A8"},
-			"title3": {"value": "通知时间:\t\t\t\t", "color":"#A8A8A8"},
-			"title4": {"value": "备注:\t\t\t\t", "color":"#A8A8A8"},
-			"content1": {"value": "{}\n".format(msg_content)},
-			"content2": {"value": "{}\n".format(today)},
-			"content3": {"value":"回复任意消息重新启用文本通知模式\n"}
-    		}
-    		res = wm.send_template(user_ids[i], template_ids[i], data)
-    		print(res)
+            return false
